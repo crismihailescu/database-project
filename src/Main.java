7de997f5eef18c8fe46bcf28444d1ebf11a49c03
cris-mihai <cris12we4@gmail.com>
2020-03-29 18:10:48 -0700

    Created tables

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import tableCreation.createNewTables;
+
 import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        createNewTables createNewTables = new createNewTables();
         databaseLayout myDatabasePanel = new databaseLayout();
         myDatabasePanel.setVisible(true);
     }
